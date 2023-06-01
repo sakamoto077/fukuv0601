28d16b12923cb479cf6f79d5b45e27f8a623bd8f
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-06-01 13:46:02 +0900

    復習

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -3,31 +3,32 @@ namespace fukuv0601
     public partial class Form1 : Form
     {
         int vx = 10;
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            button1.Left = -10;
+        {            
             int vx = -10;
         }
 
         private void button2_Click(object sender, EventArgs e)
-        {
-            button2.Left = 10;
+        {           
             int vx = 10;
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
                timer1.Stop();
+            MessageBox.Show("ç‚ñ{Å@ïj");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+            button1.Left += 10;
+            button2.Left -= 10;
         }
     }
 }
